5f3e8df0db0b119ca851fa9693297ac87b6537a6
cjscript <anzac98765@yahoo.com>
2014-04-20 10:10:32 -0700

    update

--- src/AxonMedicine/WhiteKoatBundle/Controller/ImportDrugDataController.php
+++ src/AxonMedicine/WhiteKoatBundle/Controller/ImportDrugDataController.php
@@ -101,12 +101,10 @@ class ImportDrugDataController extends GenericController
     private function process($original, $new)
     {
         // parse alias data...
-//        $databaseFile = dirname(dirname(__FILE__)) . '/Database/wk_db82332_1Qxdf/whitekoat.sql';
-$databaseFile = '/WK/Database/wk_db82332_1Qxdf/whitekoat.sql';
+        $databaseFile = dirname(dirname(__FILE__)) . '/Database/wk_db82332_1Qxdf/whitekoat.sql';
+//$databaseFile = '/WK/Database/wk_db82332_1Qxdf/whitekoat.sql';
 //        echo 'database file: ' . $databaseFile;
         $this->refreshDb($databaseFile);
-        
-        exit;
         
         // parse alias data...
         (new AliasDataParser($this))->parse($original, $new);
