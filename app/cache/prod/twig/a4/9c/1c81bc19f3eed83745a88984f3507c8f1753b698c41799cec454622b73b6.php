09e2d324932022e4b67524ba96289fa7e4ce03a7
cjscript <anzac98765@yahoo.com>
2014-04-26 17:33:40 -0700

    updated schema

--- app/cache/prod/twig/a4/9c/1c81bc19f3eed83745a88984f3507c8f1753b698c41799cec454622b73b6.php
+++ app/cache/prod/twig/a4/9c/1c81bc19f3eed83745a88984f3507c8f1753b698c41799cec454622b73b6.php
@@ -34,7 +34,7 @@ class __TwigTemplate_a49c1c81bc19f3eed83745a88984f3507c8f1753b698c41799cec454622
         <div class=\"spacer\"></div>
         <div class=\"cardTabContainer\">
             <div class=\"imageContent\">
-\t\t";
+            \t";
         // line 16
         $context['_parent'] = (array) $context;
         $context['_seq'] = twig_ensure_traversable((isset($context["images"]) ? $context["images"] : $this->getContext($context, "images")));
@@ -53,25 +53,27 @@ class __TwigTemplate_a49c1c81bc19f3eed83745a88984f3507c8f1753b698c41799cec454622
         }
         foreach ($context['_seq'] as $context["_key"] => $context["image"]) {
             // line 17
-            echo "                <a href=\"./images_3921343193822/";
+            echo "                <a href=\"/images_3921343193822/";
             echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute((isset($context["image"]) ? $context["image"] : $this->getContext($context, "image")), "imageref"), "id"), "html", null, true);
             echo ".";
             echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute((isset($context["image"]) ? $context["image"] : $this->getContext($context, "image")), "imageref"), "originalfileext"), "html", null, true);
             echo "\" data-lightbox=\"";
             echo twig_escape_filter($this->env, $this->getAttribute((isset($context["libValue"]) ? $context["libValue"] : $this->getContext($context, "libValue")), "name"), "html", null, true);
-            echo "\"><img src=\"../images_3921343193822/";
+            echo "\">
+                    <img src=\"/images_3921343193822/";
+            // line 18
             echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute((isset($context["image"]) ? $context["image"] : $this->getContext($context, "image")), "imageref"), "id"), "html", null, true);
             echo "_thmb.";
             echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute((isset($context["image"]) ? $context["image"] : $this->getContext($context, "image")), "imageref"), "originalfileext"), "html", null, true);
             echo "\"></a>
                     ";
-            // line 18
+            // line 19
             if (((0 == $this->getAttribute((isset($context["loop"]) ? $context["loop"] : $this->getContext($context, "loop")), "index") % 2) && (!$this->getAttribute((isset($context["loop"]) ? $context["loop"] : $this->getContext($context, "loop")), "last")))) {
-                // line 19
+                // line 20
                 echo "                <br>
                     ";
             }
-            // line 21
+            // line 22
             echo "\t\t";
             ++$context['loop']['index0'];
             ++$context['loop']['index'];
@@ -85,7 +87,7 @@ class __TwigTemplate_a49c1c81bc19f3eed83745a88984f3507c8f1753b698c41799cec454622
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['image'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 22
+        // line 23
         echo "            </div>
         </div>
     </div>
@@ -104,6 +106,6 @@ class __TwigTemplate_a49c1c81bc19f3eed83745a88984f3507c8f1753b698c41799cec454622
 
     public function getDebugInfo()
     {
-        return array (  89 => 22,  75 => 21,  71 => 19,  69 => 18,  56 => 17,  39 => 16,  26 => 6,  19 => 1,);
+        return array (  91 => 23,  77 => 22,  73 => 20,  71 => 19,  65 => 18,  56 => 17,  39 => 16,  26 => 6,  19 => 1,);
     }
 }
